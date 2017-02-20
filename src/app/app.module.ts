cb51a5a5cc0bc5a89f23a54dba44e7008448cde0
Paloma <paloma.pereira@outlook.com>
2017-02-20 20:53:04 -0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
     imports: [ BrowserModule ],
-    declarations: [AppComponent ],
-    bootstrap: [AppComponent]
+    declarations: [ AppComponent ],
+    bootstrap: [ AppComponent]
 })
 
 export class AppModule { }
