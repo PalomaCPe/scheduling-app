c127cc2f5b1bc20bf759cc1b1f8cf12ad279a0df
Salata <lucas.ramalho.salata@avanade.com>
2017-02-21 19:01:06 -0300

    Removendo JS

--- src/app/booking/booking.components.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=booking.components.js.map
--- src/app/booking/booking.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var booking_module_1 = require('./booking.module');
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(booking_module_1.BookingModule);
-//# sourceMappingURL=booking.js.map
