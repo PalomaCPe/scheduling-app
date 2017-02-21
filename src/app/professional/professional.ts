8cbb24c21c60025b38661e08766f9dcf95c60be4
Enoque Leal <enoquefelipe@gmail.com>
2017-02-21 19:50:57 -0300

    Criei a entidade professional.ts

---
+++ src/app/professional/professional.ts
@@ -0,0 +1,10 @@
+export class Professional {
+    propriedade: string = "Professional Hello world"; 
+    id: number;
+    pid: number; 
+    eid: number; 
+    name: string;
+    email: string; 
+    phone: number;
+    role: string;
+ }
