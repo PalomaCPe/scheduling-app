51543c17a3d697ad0770d963f05ffabba9802bcf
Rafael Zendron Lopes <rafael.zendron.lopes@avanade.com>
2017-02-21 19:09:24 -0300

    Criação Professional

---
+++ src/app/Professional/professional.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'ava-professional' ,
+    templateUrl: 'professional.html'
+})
+
+export class ProfessionalComponent
+{
+
+}
