1a8a8f79905d4036086a85276b10a3d1e259d9c8
= <euripedes.silva@avanade.com>
2017-02-21 19:53:00 -0300

    Role

---
+++ src/app/role/role.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'role' ,
+    templateUrl: 'role.html'
+})
+
+export class RoleComponent{ }
---
+++ src/app/role/role.ts
@@ -0,0 +1,9 @@
+export class Role
+{
+    id: number;
+    name: string;
+    brc: string;
+    description:string;
+    level:number;   
+
+}
