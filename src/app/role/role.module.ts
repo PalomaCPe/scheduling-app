29fcbea735ac331f9b9fcacba44d3fdd00bb2dec
Enoque Leal <enoquefelipe@gmail.com>
2017-02-21 19:01:02 -0300

    Adicionei os modulos das novas entidades

---
+++ src/app/role/role.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+    imports: [ BrowserModule ],
+    declarations: [ AppComponent, CustomerComponent, BookingComponent, RoleComponent, ProfessionalComponent, ProjectComponent], 
+    bootstrap: [ AppComponent, CustomerComponent, BookingComponent, RoleComponent, ProfessionalComponent, ProjectComponent]
+})
+
+export class RoleModule { }
