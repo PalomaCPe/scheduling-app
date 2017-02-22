a48f08dd0a4a843f03dca9045c8917be82e0cfbd
Léo <leo.weverton.nunesdossantos@gmail.com>
2017-02-21 22:03:54 -0300

    boonkin modulo

--- src/app/booking/booking.module.ts
+++ src/app/booking/booking.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BookingComponent } from './booking.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-    imports: [ BrowserModule ],
-    declarations: [ BookingComponent], 
-    bootstrap: [ BookingComponent]
+    imports: [ BrowserModule,
+        RouterModule.forChild([
+            {path: 'booking',component: BookingComponent}
+        ])
+     ],
+    declarations: [ BookingComponent]
 })
 
 export class BookingModule { }
