81c85f39f36c7f11618451c2ecff3675ce740475
Goluka <mmneto54@gmail.com>
2017-02-21 21:44:49 -0300

    Incluido o diretorio project

---
+++ src/app/project/project.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'ava-app' ,
+    templateUrl: 'app.html'
+})
+
+export class AppComponent{ 
+    propriedade: string = ' Teste aki  ';
+
+}
