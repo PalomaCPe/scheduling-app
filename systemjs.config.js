7a275507d0813c0a801f9b614f3e3817f3dcbdb9
Paloma <paloma.pereira@outlook.com>
2017-02-22 18:02:38 -0300

    Configs.

--- systemjs.config.js
+++ systemjs.config.js
@@ -11,7 +11,7 @@
     // map tells the System loader where to look for things
     map: {
       // our app is within the app folder
-      app: 'app',
+      app: 'src/app',
 
       // angular bundles
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
@@ -30,7 +30,7 @@
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
       app: {
-        main: '../src/app/main.js',
+        main: '../app/main.js',
         defaultExtension: 'js'
       },
       rxjs: {
