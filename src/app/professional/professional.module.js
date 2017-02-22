d891678fdcb695a6399bb74dc3a4cca0d18ad565
Luiz Dias <luizkdias@gmail.com>
2017-02-21 21:35:18 -0300

    Adicionando cliente.

--- src/app/professional/professional.module.js
+++ src/app/professional/professional.module.js
@@ -10,15 +10,17 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
+var router_1 = require('@angular/router');
 var professional_component_1 = require('./professional.component');
 var ProfessionalModule = (function () {
     function ProfessionalModule() {
     }
     ProfessionalModule = __decorate([
         core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule],
+            imports: [platform_browser_1.BrowserModule, router_1.RouterModule.forChild([
+                    { path: 'professionals', component: professional_component_1.ProfessionalComponent }
+                ])],
             declarations: [professional_component_1.ProfessionalComponent],
-            bootstrap: [professional_component_1.ProfessionalComponent]
         }), 
         __metadata('design:paramtypes', [])
     ], ProfessionalModule);
