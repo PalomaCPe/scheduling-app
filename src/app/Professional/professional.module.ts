1d3893eadac81293d8d2dacba3aa0e9a8e28cf0d
Rafael Zendron Lopes <rafael.zendron.lopes@avanade.com>
2017-02-21 21:31:02 -0300

    Professional

--- src/app/Professional/professional.module.ts
+++ src/app/Professional/professional.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule, Route } from '@angular/router';
+
 import { ProfessionalComponent } from './professional.component';
 
 @NgModule({
-    imports: [ BrowserModule ],
+    imports: [ BrowserModule,
+    RouterModule.forChild([
+        { path: 'professionals', component: ProfessionalComponent }
+    ])],
     declarations: [ ProfessionalComponent ],
     bootstrap: [ ProfessionalComponent]
 })
