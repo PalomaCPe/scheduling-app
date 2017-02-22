b5bdeaa628603cf343bd8b7908f0b195dc11b2a6
Salata <lucas.ramalho.salata@avanade.com>
2017-02-21 21:11:27 -0300

    Atualização Rotas

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -10,13 +10,16 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
+var router_1 = require('@angular/router');
+var booking_module_1 = require('./booking/booking.module');
 var app_component_1 = require('./app.component');
 var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule],
+            imports: [platform_browser_1.BrowserModule, booking_module_1.BookingModule, router_1.RouterModule
+                    .forRoot([{ path: '', redirectTo: "/", pathMatch: 'full' }])],
             declarations: [app_component_1.AppComponent],
             bootstrap: [app_component_1.AppComponent]
         }), 
