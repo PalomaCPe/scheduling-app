ae79e87ed4586e6585e88631f29bc04540a813ed
Salata <lucas.ramalho.salata@avanade.com>
2017-02-23 20:16:11 -0300

    Detalhes da Alocação

--- src/app/booking/booking.component.js
+++ src/app/booking/booking.component.js
@@ -24,10 +24,9 @@ var BookingComponent = (function () {
             selector: 'ava-booking',
             templateUrl: 'booking.html'
         }), 
-        __metadata('design:paramtypes', [(typeof (_a = typeof booking_service_1.BookingService !== 'undefined' && booking_service_1.BookingService) === 'function' && _a) || Object])
+        __metadata('design:paramtypes', [booking_service_1.BookingService])
     ], BookingComponent);
     return BookingComponent;
-    var _a;
 }());
 exports.BookingComponent = BookingComponent;
 //# sourceMappingURL=booking.component.js.map
