2e7f243eb888ce4c3d932287dbf8501441148440
Douglas <d.masson.soares@avanade.com>
2017-02-22 22:16:05 -0300

    Pipi custom

--- src/app/booking/booking.module.ts
+++ src/app/booking/booking.module.ts
@@ -3,15 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BookingComponent } from './booking.component';
 import { RouterModule } from '@angular/router';
 import { BookingService } from './booking.service';
+import { PercentualPipe } from '../shared/percentualPipe';
 
 @NgModule({
     imports: [BrowserModule,
-    // ROTA
-    RouterModule.forChild([
-        { path: 'bookings', component: BookingComponent}
-    ])],
-    declarations: [ BookingComponent],
+        // ROTA
+        RouterModule.forChild([
+            { path: 'bookings', component: BookingComponent }
+        ])],
+    declarations: [BookingComponent, PercentualPipe],
     providers: [BookingService]
 })
 
-export class BookingModule {  }
+export class BookingModule { }
